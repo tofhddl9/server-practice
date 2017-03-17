fe2da1c6fbf7bb219b607f2d0beaf6e31bdf7c5a
Gwanghun <tofhddl9@korea.ac.kr>
2017-03-17 17:56:13 +0900

    Fix tab

--- tcp_echo_client.cpp
+++ tcp_echo_client.cpp
@@ -7,46 +7,46 @@
 
 int main(int argc, char* argv[])
 {
-	struct sockaddr_in serv_addr, clnt_addr;
-	int clnt_sock, i;
-	int send_msg_len;
-	int total_recvbyte = 0, recvbyte;
-	char* serv_ip,*send_msg,recv_msg[RECV_BUFSIZE]; // recv msg 초기화 해주나? 어딨누 똥같은넘아
-	unsigned short serv_port;
+  struct sockaddr_in serv_addr, clnt_addr;
+  int clnt_sock, i;
+  int send_msg_len;
+  int total_recvbyte = 0, recvbyte;
+  char* serv_ip,*send_msg,recv_msg[RECV_BUFSIZE]; // recv msg 초기화 해주나? 어딨누 똥같은넘아
+  unsigned short serv_port;
 
 
-	if (argc != 4) {
-		fprintf(stderr, "Usage : [%s] [server_ip] [server_port] [message]\n", argv[0]);
-		exit(1);
- 	}
-	serv_ip = argv[1];
-	serv_port = atoi(argv[2]);
-	send_msg = argv[3];
+  if (argc != 4) {
+    fprintf(stderr, "Usage : [%s] [server_ip] [server_port] [message]\n", argv[0]);
+    exit(1);
+  }
+  serv_ip = argv[1];
+  serv_port = atoi(argv[2]);
+  send_msg = argv[3];
 
-	clnt_sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (clnt_sock < 0){
+  clnt_sock = socket(PF_INET, SOCK_STREAM, 0);
+  if (clnt_sock < 0){
     perror("socket() error\n")
-		exit(1);
-	}
+    exit(1);
+  }
 
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(serv_ip);
-	serv_addr.sin_port = htons(serv_port);
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(serv_ip);
+  serv_addr.sin_port = htons(serv_port);
 
-	if (connect(clnt_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+  if (connect(clnt_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
     perror("connect() error");
-		exit(1);
-	}
+    exit(1);
+  }
 
-	send_msg_len = strlen(send_msg);
-	for (i=0;i<100;i++) {
-	//  이렇게 하면 총 10번 메세지 보내겠지?
-		total_recvbyte = 0;
-		if (send(clnt_sock, send_msg, send_msg_len, 0) != send_msg_len){
-	 	perror("send() error");
-		exit(1);
-	}
+  send_msg_len = strlen(send_msg);
+  for (i=0;i<100;i++) {
+  //  이렇게 하면 총 10번 메세지 보내겠지?
+    total_recvbyte = 0;
+    if (send(clnt_sock, send_msg, send_msg_len, 0) != send_msg_len){
+    perror("send() error");
+    exit(1);
+  }
 
     while (total_recvbyte < send_msg_len) {
       if ((recvbyte = recv(clnt_sock, recv_msg, RECV_BUFSIZE-1, 0)) <= 0){
